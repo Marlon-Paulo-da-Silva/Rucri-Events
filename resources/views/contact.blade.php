66fea72f22c807041b0b840073dd190afb1e6985
unknown <marlon.paulo.silva@outlook.com>
2021-12-11 09:48:03 -0300

    Inicializando a configuração do Layout

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title', 'Pagina de contato')
+
+@section('content')
+<h1>Essa é a pagina de contato</h1>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $nome = "Marlon";
+
+    $arr = [1, 2, 3, 4, 5];
+
+    return view('welcome',
+        ['nome' => $nome,
+        'idade' => 24,
+        'profissao' => 'programador',
+        'arr' => $arr
+    ]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
