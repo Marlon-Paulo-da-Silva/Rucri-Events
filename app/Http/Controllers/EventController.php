fc9fd995447ec4d9f6a2ce5a66d3c13fea3b48dc
Marlon-Paulo-Da-Silva <marlon.paulo.silva@outlook.com>
2021-12-14 12:20:27 -0300

    adicionando o botão de upload de imagens

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -27,6 +27,20 @@ class EventController extends Controller
         $event->private = $request->private;
         $event->description = $request->description;
 
+        // Image upload
+        if($request->hasFile('image') && $request->file('image')->isValid()){
+            $requestImage = $request->image;
+
+            $extension = $requestImage->getClientOriginalExtension();
+
+
+            $imageName =  md5($requestImage->getClientOriginalName()) . strtotime("now") . "." . $extension;
+
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $event->image = $imageName;
+        }
+
         $event->save();
 
         return redirect('/')->with('msg', 'Evento criado com sucesso');
